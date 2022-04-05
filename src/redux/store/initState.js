66da548abd5f549b691ad082ce3cea27aae36bbb
mavirt <mavirt@iu.edu>
2022-04-05 14:56:35 -0400

    first commit

---
+++ src/redux/store/initState.js
@@ -0,0 +1,19 @@
+function initState(){
+    return {
+        guesses: [
+            ['','','','',''],
+            ['','','','',''],
+            ['','','','',''],
+            ['','','','',''],
+            ['','','','',''],
+            ['','','','','']
+        ],
+        try:0,
+        answer:["t","e","a","c","h"],
+        change: false,
+        end:false,
+        win:false
+    }
+}
+
+export default initState;
